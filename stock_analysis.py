b5902cfc8a4fe8464a101b92bb1181d3e4748aac
Kestrel86 <90483954+Kestrel86@users.noreply.github.com>
2024-12-03 20:23:49 -0800

    modified to function

--- stock_analysis.py
+++ stock_analysis.py
@@ -134,7 +134,7 @@ plt.plot(valid_nvda[['Close', 'Predictions']])
 plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
 plt.show()
 
-# Stock Prediction using influence of other Stocks in LSTM
+# Stock Prediction using influence of other Stocks in LSTM -------------------------------------------
 
 # stock prediction focuses semiconductor stocks
 
@@ -145,84 +145,88 @@ plt.show()
 stock_list = ['NVDA','TSM', 'AVGO', 'QCOM', 'AMD']
 
 target_symbol = 'NVDA'
+def stock_model(stock_list, target_symbol):
+    # Fetch data for multiple stocks for 5 years
+    end = datetime.now()
+    start = datetime(end.year - 5, end.month, end.day)
 
-# Fetch data for multiple stocks for 5 years
-end = datetime.now()
-start = datetime(end.year - 5, end.month, end.day)
+    # Fetch data for multiple stocks for the last 5 years
+    data = yf.download(stock_list, start=start, end=end)
 
-# Fetch data for multiple stocks for the last 5 years
-data = yf.download(stock_list, start=start, end=end)
+    # data.head()
 
-# data.head()
+    stock_data = data['Close']
 
-stock_data = data['Close']
+    # Separate the target stock (NVDA) and other stocks
+    target_data = stock_data[target_symbol]  # The target stock data (NVDA)
+    other_stocks_data = stock_data.drop(columns=[target_symbol])  # All other stocks
 
-# Separate the target stock (NVDA) and other stocks
-target_data = stock_data[target_symbol]  # The target stock data (NVDA)
-other_stocks_data = stock_data.drop(columns=[target_symbol])  # All other stocks
+    # Combine the data: We want the target stock's history based on all other stocks
+    dataset = pd.concat([other_stocks_data, target_data], axis=1).values
 
-# Combine the data: We want the target stock's history based on all other stocks
-dataset = pd.concat([other_stocks_data, target_data], axis=1).values
+    # Calculate the length for training data
+    training_data_len = int(np.ceil(len(dataset) * .95))
 
-# Calculate the length for training data
-training_data_len = int(np.ceil(len(dataset) * .95))
+    # Scale the data using MinMaxScaler
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(dataset)
 
-# Scale the data using MinMaxScaler
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(dataset)
+    window_size = 60 # number of days to consider
 
-window_size = 60 # number of days to consider
-
-# Prepare the feature (X) and target (y) sequences
-X, y = [], []
-for i in range(len(scaled_data) - window_size):
-    X.append(scaled_data[i:i+window_size, :-1])  # All stock data for the window, excluding target stock
-    y.append(scaled_data[i+window_size, -1])  # Target stock's next day close price, the last column
+    # Prepare the feature (X) and target (y) sequences
+    X, y = [], []
+    for i in range(len(scaled_data) - window_size):
+        X.append(scaled_data[i:i+window_size, :-1])  # All stock data for the window, excluding target stock
+        y.append(scaled_data[i+window_size, -1])  # Target stock's next day close price, the last column
 
 
-# Convert the lists into numpy arrays
-X, y = np.array(X), np.array(y)
+    # Convert the lists into numpy arrays
+    X, y = np.array(X), np.array(y)
 
-# Split the data into training and testing sets (80% train, 20% test)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Split the data into training and testing sets (80% train, 20% test)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = Sequential([
-    LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
-    LSTM(50, return_sequences=False),
-    Dense(25),
-    Dense(1)
-])
+    model = Sequential([
+        LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], X_train.shape[2])),
+        LSTM(50, return_sequences=False),
+        Dense(25),
+        Dense(1)
+    ])
 
-model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='mse')
 
-epochs = 100
-batch_size = 32
+    epochs = 100
+    batch_size = 32
 
-# Train the model
-model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test), verbose=0)
+    # Train the model
+    model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test), verbose=0)
 
-# Make predictions
-test_data = scaled_data[training_data_len - window_size:, :]
-test_set = []
-for i in range(window_size, len(test_data)):
-    test_set.append(test_data[i - window_size:i, :-1])  # Use the history of other stocks
+    # Make predictions
+    test_data = scaled_data[training_data_len - window_size:, :]
+    test_set = []
+    for i in range(window_size, len(test_data)):
+        test_set.append(test_data[i - window_size:i, :-1])  # Use the history of other stocks
 
-test_set = np.array(test_set)
-test_set = np.reshape(test_set, (test_set.shape[0], test_set.shape[1], test_set.shape[2]))
+    test_set = np.array(test_set)
+    test_set = np.reshape(test_set, (test_set.shape[0], test_set.shape[1], test_set.shape[2]))
 
-# Prediction
-prediction = model.predict(test_set)
+    # Prediction
+    prediction = model.predict(test_set)
 
-# Reshape the prediction to (num_samples, 1) for inverse transformation
-prediction = prediction.reshape(-1, 1)  # Flatten the output for inverse transformation
+    # Reshape the prediction to (num_samples, 1) for inverse transformation
+    prediction = prediction.reshape(-1, 1)  # Flatten the output for inverse transformation
 
-# Inverse transform the predictions for the target stock (the last column)
-scaled_pred = scaler.inverse_transform(np.hstack([np.zeros((prediction.shape[0], scaled_data.shape[1] - 1)), prediction]))
+    # Inverse transform the predictions for the target stock (the last column)
+    scaled_pred = scaler.inverse_transform(np.hstack([np.zeros((prediction.shape[0], scaled_data.shape[1] - 1)), prediction]))
 
-# Prepare results for the target stock
-valid = target_data[training_data_len:]
-valid = valid.to_frame()  # Convert to DataFrame, in the case the data is a series
-valid['Predictions'] = scaled_pred[:, -1]  # Only take the last column for the target stock
+    # Prepare results for the target stock
+    valid = target_data[training_data_len:]
+    valid = valid.to_frame()  # Convert to DataFrame, in the case the data is a series
+    valid['Predictions'] = scaled_pred[:, -1]  # Only take the last column for the target stock
+
+    return valid, training_data_len
+
+valid, training_data_len, data = stock_model(stock_list, stock_symbol)
 
 r2 = r2_score(valid['NVDA'], valid['Predictions'])
 print(f'R2 Score: {r2}')
